f304edae139e2d19881ec9ded45dd3bc35d9177a
지은성 <ddrr2852@gmail.com>
2025-12-30 11:01:33 +0900

    feat :image 파일 body에 합침

--- lib/pages/product_detail/product_detail_page.dart
+++ lib/pages/product_detail/product_detail_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tech_check_app/core/widgets/common_app_bar.dart';
 import 'package:tech_check_app/pages/product_detail/widgets/detail_body.dart';
 import 'package:tech_check_app/pages/product_detail/widgets/detail_bottom.dart';
-import 'package:tech_check_app/pages/product_detail/widgets/detail_image.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({super.key});
@@ -13,7 +12,6 @@ class DetailPage extends StatelessWidget {
       appBar: CommonAppBar(),
       body: Column(
         children: [
-          DetailImage(),
           DetailBody(),
           //
         ],
